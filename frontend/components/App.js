2db41221c5d9e367e6377c2674a00962c561f154
Dominic A.N. <dominicnunez@gmail.com>
2024-10-30 12:11:23 -0500

    write functions for App.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -18,8 +18,8 @@ export default function App() {
 
   // ✨ Research `useNavigate` in React Router v.6
   const navigate = useNavigate()
-  const redirectToLogin = () => { /* ✨ implement */ }
-  const redirectToArticles = () => { /* ✨ implement */ }
+  const redirectToLogin = () => { navigate('/') }
+  const redirectToArticles = () => { navigate('/articles') }
 
   const logout = () => {
     // ✨ implement
@@ -27,6 +27,13 @@ export default function App() {
     // and a message saying "Goodbye!" should be set in its proper state.
     // In any case, we should redirect the browser back to the login screen,
     // using the helper above.
+    localStorage.removeItem('token')
+    redirectToLogin()
+  }
+
+  const isLoading = () => {
+    setMessage('')
+    setSpinnerOn(true)
   }
 
   const login = ({ username, password }) => {
@@ -36,6 +43,32 @@ export default function App() {
     // On success, we should set the token to local storage in a 'token' key,
     // put the server success message in its proper state, and redirect
     // to the Articles screen. Don't forget to turn off the spinner!
+    isLoading()
+    fetch(loginUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    })
+      .then(res => {
+        if (res.status === 200) {
+          return res.json()
+        } else {
+          throw new Error('Login failed')
+        }
+      })
+      .then(data => {
+        localStorage.setItem('token', data.token)
+        setMessage(data.message)
+        redirectToArticles()
+      })
+      .catch(err => {
+        throw new Error(err.message)
+      })
+      .finally(() => {
+        setSpinnerOn(false)
+      })
   }
 
   const getArticles = () => {
@@ -47,6 +80,32 @@ export default function App() {
     // If something goes wrong, check the status of the response:
     // if it's a 401 the token might have gone bad, and we should redirect to login.
     // Don't forget to turn off the spinner!
+    isLoading()
+    fetch(articlesUrl, {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+      .then(res => {
+        if (res.status === 200) {
+          return res.json()
+        } else if (res.status === 401) {
+          localStorage.removeItem('token')
+          redirectToLogin()
+        } else {
+          throw new Error('Failed to fetch articles')
+        }
+      })
+      .then(data => {
+        setArticles(data)
+        setMessage(data.message)
+      })
+      .catch(err => {
+        throw new Error(err.message)
+      })
+      .finally(() => {
+        setSpinnerOn(false)
+      })
   }
 
   const postArticle = article => {
@@ -54,15 +113,110 @@ export default function App() {
     // The flow is very similar to the `getArticles` function.
     // You'll know what to do! Use log statements or breakpoints
     // to inspect the response from the server.
+    fetch(articlesUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      },
+      body: JSON.stringify(article)
+    })
+      .then(res => {
+        if (res.status === 201) {
+          return res.json()
+        } else if (res.status === 401) {
+          localStorage.removeItem('token')
+          redirectToLogin()
+        } else {
+          throw new Error('Failed to post article')
+        }
+      })
+      .then(data => {
+        setArticles([...articles, data])
+        setMessage(data.message)
+      })
+      .catch(err => {
+        throw new Error(err.message)
+      })
+      .finally(() => {
+        setSpinnerOn(false)
+      })
+  }
+
+  const articleURL = (article_id) => {
+    return `${articlesUrl}/${article_id}`
   }
 
   const updateArticle = ({ article_id, article }) => {
+    const updateURL = articleURL(article_id)
     // ✨ implement
     // You got this!
+    fetch(updateURL, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      },
+      body: JSON.stringify(article)
+    })
+      .then(res => {
+        if (res.status === 200) {
+          return res.json()
+        } else if (res.status === 401) {
+          localStorage.removeItem('token')
+          redirectToLogin()
+        } else {
+          throw new Error('Failed to update article')
+        }
+      })
+      .then(data => {
+        const updatedArticles = articles.map(a => {
+          if (a.article_id === article_id) {
+            return data
+          }
+          return a
+        })
+        setArticles(updatedArticles)
+        setMessage(data.message)
+      })
+      .catch(err => {
+        throw new Error(err.message)
+      })
+      .finally(() => {
+        setSpinnerOn(false)
+      })
   }
 
   const deleteArticle = article_id => {
     // ✨ implement
+    const deleteURL = articleURL(article_id)
+    fetch(deleteURL, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+      .then(res => {
+        if (res.status === 200) {
+          return res.json()
+        } else if (res.status === 401) {
+          localStorage.removeItem('token')
+          redirectToLogin()
+        } else {
+          throw new Error('Failed to delete article')
+        }
+      })
+      .then(data => {
+        const updatedArticles = articles.filter(a => a.article_id !== article_id)
+        setArticles(updatedArticles)
+        setMessage(data.message)
+      })
+      .catch(err => {
+        throw new Error(err.message)
+      })
+      .finally(() => {
+        setSpinnerOn(false)
+      })
   }
 
   return (
