5bbe569ba4e29c70b55aa6c9acc931f800296072
Dominic A.N. <dominicnunez@gmail.com>
2024-11-04 13:18:10 -0600

    return user without auth to login

--- frontend/components/Articles.js
+++ frontend/components/Articles.js
@@ -14,6 +14,9 @@ export default function Articles(props) {
 
   // ✨ implement conditional logic: if no token exists
   // we should render a Navigate to login screen (React Router v.6)
+  if (!localStorage.getItem('token')) {
+    return <Navigate to="/login" />
+  }
 
   useEffect(() => {
     // ✨ grab the articles here, on first render only
